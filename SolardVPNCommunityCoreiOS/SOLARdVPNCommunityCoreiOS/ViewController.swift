d9187a349b161b3eccbfa3477cfd27aa55fd8da3
Viktoriia Kostyleva <kostyleva.viktoriya@gmail.com>
2022-09-26 11:52:22 +0300

    Project init

---
+++ SolardVPNCommunityCoreiOS/SOLARdVPNCommunityCoreiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOLARdVPNCommunityCoreiOS
+//
+//  Created by Viktoriia Kostyleva on 26.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
