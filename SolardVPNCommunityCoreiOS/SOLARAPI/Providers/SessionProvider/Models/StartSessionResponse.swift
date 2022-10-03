4f3ec6ec951af14c4d74fcb72d75c96dbdd98c2b
Lika Vorobeva <lika@solarlabs.ee>
2022-10-03 16:36:38 +0300

    #4: Add nodes service provider

---
+++ SolardVPNCommunityCoreiOS/SOLARAPI/Providers/SessionProvider/Models/StartSessionResponse.swift
@@ -0,0 +1,13 @@
+//
+//  StartSessionResponse.swift
+//  SOLARAPI
+//
+//  Created by Lika Vorobeva on 26.09.2022.
+//
+
+import Foundation
+
+public struct StartSessionResponse: Codable {
+    public let success: Bool
+    public let result: String?
+}
