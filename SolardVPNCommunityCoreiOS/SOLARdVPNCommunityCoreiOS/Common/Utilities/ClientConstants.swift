d0a896d09eb6274fc2836f2d0e1e59564e9d19f4
Lika Vorobeva <lika@solarlabs.ee>
2022-10-03 16:38:05 +0300

    #4: Add ClientConnectionConfigurationType

--- SolardVPNCommunityCoreiOS/SOLARdVPNCommunityCoreiOS/Common/Utilities/ClientConstants.swift
+++ SolardVPNCommunityCoreiOS/SOLARdVPNCommunityCoreiOS/Common/Utilities/ClientConstants.swift
@@ -6,12 +6,25 @@
 //
 
 import Foundation
+import SentinelWallet
 
 enum ClientConstants {
     static let host = "localhost"
     static let port = 3876
     
+    static let defaultLCDHostString = "lcd-sentinel.dvpn.solar"
+    static let defaultLCDPort = 993
+    
     static let apiPath = "api"
     
     static let backendURL = URL(string: "https://BACKEND")!
+}
+
+final class ApplicationConfiguration: ClientConnectionConfigurationType {
+    private(set) static var shared = ApplicationConfiguration()
+    
+    var grpcMirror: ClientConnectionConfiguration = .init(
+        host: ClientConstants.defaultLCDHostString,
+        port: ClientConstants.defaultLCDPort
+    )
 }
